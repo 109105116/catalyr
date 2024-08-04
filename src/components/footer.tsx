b04c37d808d5bbfeb2b7c3803ed8807f83466170
109105116 <charliewang109105116@gmail.com>
2024-08-04 12:42:21 -0400

    fix user undefined case in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,11 +6,14 @@ import { db } from "@/lib/db";
 
 export async function Footer() {
   const session = await getAuthSession();
-  const user = await db.user.findUnique({
-    where: {
-      id: session?.user.id,
-    },
-  });
+  let user = null;
+  if (session?.user.id) {
+    user = await db.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+    });
+  }
 
   return (
     <footer className="fixed bottom-0 bg-background w-full py-1 flex justify-center border-t-[1px] border-border text-muted-foreground text-s font-light">
